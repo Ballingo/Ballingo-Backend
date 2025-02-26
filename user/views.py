dbb662045ec34249b137a7cfee9b7cae9c40aa12
Santiago Santana Martínez <pimisan@gmail.com>
2025-02-26 23:50:13 +0000

    feat(User): Add token when sign-up

--- user/views.py
+++ user/views.py
@@ -11,6 +11,22 @@ class SignupView(generics.CreateAPIView):
     serializer_class = BallingoUserSerializer
     permission_classes = [permissions.AllowAny]
 
+    def post(self, request):
+        serializer = BallingoUserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()  # Crea el usuario
+            token, created = Token.objects.get_or_create(user=user)  # Genera el token
+
+            return Response({
+                'token': token.key,
+                'user_id': user.id,
+                'username': user.username,
+                'email': user.email
+            }, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class LoginView(APIView):
     permission_classes = [permissions.AllowAny]
 
