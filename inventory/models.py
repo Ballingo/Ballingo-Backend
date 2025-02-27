3b8f53a5e913be00abf83d23e2c8de4daed0d416
Santiago Santana Martínez <pimisan@gmail.com>
2025-02-27 02:24:20 +0000

    fix(player and inventory): Values correction

--- inventory/models.py
+++ inventory/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Inventory(models.Model):
     clothes_inventory = models.OneToOneField('wardrobe.Wardrobe', on_delete=models.CASCADE, related_name='clothes_inventory')
     food_inventory = models.OneToOneField('foodList.FoodList', on_delete=models.CASCADE, related_name='food_inventory')
-    coins = models.IntegerField(default=0)
-    livesCounter = models.IntegerField(default=0)
+    coins = models.IntegerField(default=100)
+    livesCounter = models.IntegerField(default=3)
