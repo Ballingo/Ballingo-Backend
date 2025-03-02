ce765d0d2f6346bfca8c9f0b11f0c825c2c05df3
acaymo <acaimopcgc@gmail.com>
2025-03-02 00:29:37 +0000

    feat(trade): create trade work

--- trade/views.py
+++ trade/views.py
@@ -3,5 +3,17 @@ from .models import Trade
 from .serializers import TradeSerializer
 
 class TradeViewSet(viewsets.ModelViewSet):
-    queryset = Trade.objects.all()
     serializer_class = TradeSerializer
+
+    def get_queryset(self):
+        queryset = Trade.objects.all()
+        is_active = self.request.query_params.get("isActive")
+
+        if is_active is not None:
+            # Convertir string a booleano correctamente
+            if is_active.lower() in ["true", "1"]:
+                queryset = queryset.filter(isActive=True)
+            elif is_active.lower() in ["false", "0"]:
+                queryset = queryset.filter(isActive=False)
+
+        return queryset
