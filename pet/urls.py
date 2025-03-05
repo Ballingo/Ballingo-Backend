84e874527f6d9f2a9517874e6e9b03204431ec1f
acaymo <acaimopcgc@gmail.com>
2025-03-05 00:15:35 +0000

    feat(pet): pet style add

--- pet/urls.py
+++ pet/urls.py
@@ -5,4 +5,10 @@ from .views import PetViewSet
 router = DefaultRouter()
 router.register(r'pet', PetViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    # Ruta personalizada para obtener una mascota por player_id y language
+    path('pet/by-player/', PetViewSet.as_view({'get': 'get_pet_by_player_and_language'}), name='get_pet_by_player_and_language'),
+]
+
+# Agregar las rutas del router automáticamente generadas
+urlpatterns += router.urls
