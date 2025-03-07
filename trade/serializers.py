8c8c8f9e4ad389d256afae7244174759a4aee921
Santiago Santana Martínez <pimisan@gmail.com>
2025-03-07 20:18:49 +0000

    fix(Trade): Active trades

--- trade/serializers.py
+++ trade/serializers.py
@@ -1,21 +1,22 @@
 from rest_framework import serializers
 from .models import Trade
-from food.models import Food  # Importar modelo Food
-from food.serializers import FoodSerializer  # Importar FoodSerializer para anidar en GET
-from player.serializers import PlayerSerializer  # Importar PlayerSerializer para anidar en GET
+from food.models import Food
+from food.serializers import FoodSerializer
+from player.models import Player
+from player.serializers import PlayerSerializer
 
 class TradeSerializer(serializers.ModelSerializer):
     # Para lectura (GET), usar el serializador de Food
     in_food = FoodSerializer(read_only=True)
     out_food = FoodSerializer(read_only=True)
+    player = PlayerSerializer(read_only=True)
 
     # Para escritura (POST, PUT), permitir claves foráneas
     in_food_id = serializers.PrimaryKeyRelatedField(queryset=Food.objects.all(), source='in_food', write_only=True)
     out_food_id = serializers.PrimaryKeyRelatedField(queryset=Food.objects.all(), source='out_food', write_only=True)
-
-    player = PlayerSerializer(read_only=True)
+    player_id = serializers.PrimaryKeyRelatedField(queryset=Player.objects.all(), source='player', write_only=True)
 
     class Meta:
         model = Trade
-        fields = ['id', 'player', 'isActive', 'created_at', 'in_food', 'out_food', 'in_food_id', 'out_food_id']
+        fields = ['id', 'player', 'player_id', 'isActive', 'created_at', 'in_food', 'out_food', 'in_food_id', 'out_food_id']
         depth = 1
