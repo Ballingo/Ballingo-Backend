5605bd7b7309346719f9335d6fcc9a0573462faf
Santiago Santana Martínez <pimisan@gmail.com>
2025-03-10 18:47:13 +0000

    fix(Clothes): Model changes

--- clothes/models.py
+++ clothes/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Clothes(models.Model): # type tiene que ser un ENUM { hat, tshirt, shoes, accesories} y imagen puediendo estar a null
     type = models.CharField(
         choices=[
-            ('hat', 'Hat'), 
-            ('tshirt', 'T-Shirt'), 
-            ('shoes', 'Shoes'), 
+            ('hat', 'Hat'),
+            ('eyes', 'Eyes'),
+            ('shoes', 'Shoes'),
             ('accesories', 'Accessories')
         ], 
         max_length=20
